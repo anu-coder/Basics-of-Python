e11edb41cf186e6c9049322406105176ea046166
anurima <anurima10@gmail.com>
2021-03-20 18:17:18 +0530

    all about tuple: tuple.py

---
+++ tuple.py
@@ -0,0 +1,19 @@
+tup1 = (1,2,3,4)
+tup2 = (2,3,4,5)
+tup3 = tup1+tup2
+print(tup3)
+
+#single element tuple: use (,)
+t = (1,)
+l = [1]
+print(t)
+print(l)
+
+# tuple can be indexed 
+tup = ("HEAD", 'Head', 'head')
+
+print(tup[-1])
+print(tup[0])
+print(tup[-2])
+print(tup[1:])
+
